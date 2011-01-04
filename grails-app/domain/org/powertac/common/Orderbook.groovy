e82e07399fad065047b7c19fc2e3811a6436b1c5
Carsten Block <mail@carstenblock.org>
2011-01-04 11:14:42 +0100

    Orderbook domain class logic refactored and corresponding tests added

--- grails-app/domain/org/powertac/common/Orderbook.groovy
+++ grails-app/domain/org/powertac/common/Orderbook.groovy
@@ -27,6 +27,7 @@ class Orderbook implements Serializable {
   Product product
   Timeslot timeslot
   Boolean latest
+  BigDecimal bid0 = null
   BigDecimal bid1 = null
   BigDecimal bid2 = null
   BigDecimal bid3 = null
@@ -36,17 +37,17 @@ class Orderbook implements Serializable {
   BigDecimal bid7 = null
   BigDecimal bid8 = null
   BigDecimal bid9 = null
-  BigDecimal bid10 = null
-  BigDecimal bidSize1 = 0
-  BigDecimal bidSize2 = 0
-  BigDecimal bidSize3 = 0
-  BigDecimal bidSize4 = 0
-  BigDecimal bidSize5 = 0
-  BigDecimal bidSize6 = 0
-  BigDecimal bidSize7 = 0
-  BigDecimal bidSize8 = 0
-  BigDecimal bidSize9 = 0
-  BigDecimal bidSize10 = 0
+  BigDecimal bidSize0 = 0.0
+  BigDecimal bidSize1 = 0.0
+  BigDecimal bidSize2 = 0.0
+  BigDecimal bidSize3 = 0.0
+  BigDecimal bidSize4 = 0.0
+  BigDecimal bidSize5 = 0.0
+  BigDecimal bidSize6 = 0.0
+  BigDecimal bidSize7 = 0.0
+  BigDecimal bidSize8 = 0.0
+  BigDecimal bidSize9 = 0.0
+  BigDecimal ask0 = null
   BigDecimal ask1 = null
   BigDecimal ask2 = null
   BigDecimal ask3 = null
@@ -56,17 +57,16 @@ class Orderbook implements Serializable {
   BigDecimal ask7 = null
   BigDecimal ask8 = null
   BigDecimal ask9 = null
-  BigDecimal ask10 = null
-  BigDecimal askSize1 = 0
-  BigDecimal askSize2 = 0
-  BigDecimal askSize3 = 0
-  BigDecimal askSize4 = 0
-  BigDecimal askSize5 = 0
-  BigDecimal askSize6 = 0
-  BigDecimal askSize7 = 0
-  BigDecimal askSize8 = 0
-  BigDecimal askSize9 = 0
-  BigDecimal askSize10 = 0
+  BigDecimal askSize0 = 0.0
+  BigDecimal askSize1 = 0.0
+  BigDecimal askSize2 = 0.0
+  BigDecimal askSize3 = 0.0
+  BigDecimal askSize4 = 0.0
+  BigDecimal askSize5 = 0.0
+  BigDecimal askSize6 = 0.0
+  BigDecimal askSize7 = 0.0
+  BigDecimal askSize8 = 0.0
+  BigDecimal askSize9 = 0.0
 
   static belongsTo = [product: Product, timeslot: Timeslot, competition: Competition]
 
@@ -77,155 +77,197 @@ class Orderbook implements Serializable {
   }
 
   static constraints = {
+    id(nullable: false, blank: false, unique: true)
     competition(nullable: false)
     dateExecuted(nullable: false)
     transactionId(nullable: false)
     product(nullable: false)
     timeslot(nullable: false)
-    ask1(nullable: true, scale: 2)
-    ask2(nullable: true, scale: 2)
-    ask3(nullable: true, scale: 2)
-    ask4(nullable: true, scale: 2)
-    ask5(nullable: true, scale: 2)
-    ask6(nullable: true, scale: 2)
-    ask7(nullable: true, scale: 2)
-    ask8(nullable: true, scale: 2)
-    ask9(nullable: true, scale: 2)
-    ask10(nullable: true, scale: 2)
-    bid1(nullable: true, scale: 2)
-    bid2(nullable: true, scale: 2)
-    bid3(nullable: true, scale: 2)
-    bid4(nullable: true, scale: 2)
-    bid5(nullable: true, scale: 2)
-    bid6(nullable: true, scale: 2)
-    bid7(nullable: true, scale: 2)
-    bid8(nullable: true, scale: 2)
-    bid9(nullable: true, scale: 2)
-    bid10(nullable: true, scale: 2)
-    latest (nullable: false)
+    latest(nullable: false)
+     bid0(nullable: true, scale: Constants.DECIMALS)
+    bid1(nullable: true, scale: Constants.DECIMALS)
+    bid2(nullable: true, scale: Constants.DECIMALS)
+    bid3(nullable: true, scale: Constants.DECIMALS)
+    bid4(nullable: true, scale: Constants.DECIMALS)
+    bid5(nullable: true, scale: Constants.DECIMALS)
+    bid6(nullable: true, scale: Constants.DECIMALS)
+    bid7(nullable: true, scale: Constants.DECIMALS)
+    bid8(nullable: true, scale: Constants.DECIMALS)
+    bid9(nullable: true, scale: Constants.DECIMALS)
+    bidSize0(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    bidSize1(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    bidSize2(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    bidSize3(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    bidSize4(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    bidSize5(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    bidSize6(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    bidSize7(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    bidSize8(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    bidSize9(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    ask0(nullable: true, scale: Constants.DECIMALS)
+    ask1(nullable: true, scale: Constants.DECIMALS)
+    ask2(nullable: true, scale: Constants.DECIMALS)
+    ask3(nullable: true, scale: Constants.DECIMALS)
+    ask4(nullable: true, scale: Constants.DECIMALS)
+    ask5(nullable: true, scale: Constants.DECIMALS)
+    ask6(nullable: true, scale: Constants.DECIMALS)
+    ask7(nullable: true, scale: Constants.DECIMALS)
+    ask8(nullable: true, scale: Constants.DECIMALS)
+    ask9(nullable: true, scale: Constants.DECIMALS)
+    askSize0(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    askSize1(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    askSize2(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    askSize3(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    askSize4(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    askSize5(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    askSize6(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    askSize7(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    askSize8(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+    askSize9(nullable: false, min: 0.0, scale: Constants.DECIMALS)
+
   }
 
   static transients = ['orderbookArray']
 
-  public setOrderbookArray(BigDecimal[][][] orderbookArray) {
-    /*
-   BigDecimal[0][][] = bid
-   BigDecimal[1][][] = ask
+  /**
+   * Allows updating of the orderbook's {@code bid}, {@code bidSize}, {@code ask}, {@code
+   * askSize} fields using a three dimensional array. Internally this array is mapped
+   * to the bid1 .. bid10, bidSize1 .. bidSize10, ask1 .. ask10, askSize1 .. askSize10 fields
+   * i.e. the array itself is not persisted to the database, i.e. it is transient.
+   *
+   * Array convention:
+   *
+   * BigDecimal[0][][] = bid
+   * BigDecimal[1][][] = ask
+   *
+   * BigDecimal[][0][] = price
+   * BigDecimal[][1][] = size
+   *
+   * BigDecimal[][][0] = Level 1
+   * BigDecimal[][][1] = Level 2
+   * BigDecimal[][][...] = Level ...
+   * BigDecimal[][][9] = Level 10
+   *
+   * @param orderbookArray the three dimensional array that contains the orderbook data to be
+   * persisted
+   */
+  public void setOrderbookArray(BigDecimal[][][] orderbookArray) {
 
-   BigDecimal[][0][] = price
-   BigDecimal[][1][] = size
+    bid0 = orderbookArray[0][0][0]
+    bid1 = orderbookArray[0][0][1]
+    bid2 = orderbookArray[0][0][2]
+    bid3 = orderbookArray[0][0][3]
+    bid4 = orderbookArray[0][0][4]
+    bid5 = orderbookArray[0][0][5]
+    bid6 = orderbookArray[0][0][6]
+    bid7 = orderbookArray[0][0][7]
+    bid8 = orderbookArray[0][0][8]
+    bid9 = orderbookArray[0][0][9]
 
-   BigDecimal[][][0] = Level 1
-   BigDecimal[][][1] = Level 2
-   BigDecimal[][][...] = Level ...
-   BigDecimal[][][9] = Level 10
-    */
+    bidSize0 = orderbookArray[0][1][0] == null ? 0 : orderbookArray[0][1][0]
+    bidSize1 = orderbookArray[0][1][1] == null ? 0 : orderbookArray[0][1][1]
+    bidSize2 = orderbookArray[0][1][2] == null ? 0 : orderbookArray[0][1][2]
+    bidSize3 = orderbookArray[0][1][3] == null ? 0 : orderbookArray[0][1][3]
+    bidSize4 = orderbookArray[0][1][4] == null ? 0 : orderbookArray[0][1][4]
+    bidSize5 = orderbookArray[0][1][5] == null ? 0 : orderbookArray[0][1][5]
+    bidSize6 = orderbookArray[0][1][6] == null ? 0 : orderbookArray[0][1][6]
+    bidSize7 = orderbookArray[0][1][7] == null ? 0 : orderbookArray[0][1][7]
+    bidSize8 = orderbookArray[0][1][8] == null ? 0 : orderbookArray[0][1][8]
+    bidSize9 = orderbookArray[0][1][9] == null ? 0 : orderbookArray[0][1][9]
 
+    ask0 = orderbookArray[1][0][0]
+    ask1 = orderbookArray[1][0][1]
+    ask2 = orderbookArray[1][0][2]
+    ask3 = orderbookArray[1][0][3]
+    ask4 = orderbookArray[1][0][4]
+    ask5 = orderbookArray[1][0][5]
+    ask6 = orderbookArray[1][0][6]
+    ask7 = orderbookArray[1][0][7]
+    ask8 = orderbookArray[1][0][8]
+    ask9 = orderbookArray[1][0][9]
 
-    bid1 = orderbookArray[0][0][0]
-    bid2 = orderbookArray[0][0][1]
-    bid3 = orderbookArray[0][0][2]
-    bid4 = orderbookArray[0][0][3]
-    bid5 = orderbookArray[0][0][4]
-    bid6 = orderbookArray[0][0][5]
-    bid7 = orderbookArray[0][0][6]
-    bid8 = orderbookArray[0][0][7]
-    bid9 = orderbookArray[0][0][8]
-    bid10 = orderbookArray[0][0][9]
-
-    bidSize1 = orderbookArray[0][1][0] == null ? 0 : orderbookArray[0][1][0]
-    bidSize2 = orderbookArray[0][1][1] == null ? 0 : orderbookArray[0][1][1]
-    bidSize3 = orderbookArray[0][1][2] == null ? 0 : orderbookArray[0][1][2]
-    bidSize4 = orderbookArray[0][1][3] == null ? 0 : orderbookArray[0][1][3]
-    bidSize5 = orderbookArray[0][1][4] == null ? 0 : orderbookArray[0][1][4]
-    bidSize6 = orderbookArray[0][1][5] == null ? 0 : orderbookArray[0][1][5]
-    bidSize7 = orderbookArray[0][1][6] == null ? 0 : orderbookArray[0][1][6]
-    bidSize8 = orderbookArray[0][1][7] == null ? 0 : orderbookArray[0][1][7]
-    bidSize9 = orderbookArray[0][1][8] == null ? 0 : orderbookArray[0][1][8]
-    bidSize10 = orderbookArray[0][1][9] == null ? 0 : orderbookArray[0][1][9]
-
-    ask1 = orderbookArray[1][0][0]
-    ask2 = orderbookArray[1][0][1]
-    ask3 = orderbookArray[1][0][2]
-    ask4 = orderbookArray[1][0][3]
-    ask5 = orderbookArray[1][0][4]
-    ask6 = orderbookArray[1][0][5]
-    ask7 = orderbookArray[1][0][6]
-    ask8 = orderbookArray[1][0][7]
-    ask9 = orderbookArray[1][0][8]
-    ask10 = orderbookArray[1][0][9]
-
-    askSize1 = orderbookArray[1][1][0] == null ? 0 : orderbookArray[1][1][0]
-    askSize2 = orderbookArray[1][1][1] == null ? 0 : orderbookArray[1][1][1]
-    askSize3 = orderbookArray[1][1][2] == null ? 0 : orderbookArray[1][1][2]
-    askSize4 = orderbookArray[1][1][3] == null ? 0 : orderbookArray[1][1][3]
-    askSize5 = orderbookArray[1][1][4] == null ? 0 : orderbookArray[1][1][4]
-    askSize6 = orderbookArray[1][1][5] == null ? 0 : orderbookArray[1][1][5]
-    askSize7 = orderbookArray[1][1][6] == null ? 0 : orderbookArray[1][1][6]
-    askSize8 = orderbookArray[1][1][7] == null ? 0 : orderbookArray[1][1][7]
-    askSize9 = orderbookArray[1][1][8] == null ? 0 : orderbookArray[1][1][8]
-    askSize10 = orderbookArray[1][1][9] == null ? 0 : orderbookArray[1][1][9]
+    askSize0 = orderbookArray[1][1][0] == null ? 0 : orderbookArray[1][1][0]
+    askSize1 = orderbookArray[1][1][1] == null ? 0 : orderbookArray[1][1][1]
+    askSize2 = orderbookArray[1][1][2] == null ? 0 : orderbookArray[1][1][2]
+    askSize3 = orderbookArray[1][1][3] == null ? 0 : orderbookArray[1][1][3]
+    askSize4 = orderbookArray[1][1][4] == null ? 0 : orderbookArray[1][1][4]
+    askSize5 = orderbookArray[1][1][5] == null ? 0 : orderbookArray[1][1][5]
+    askSize6 = orderbookArray[1][1][6] == null ? 0 : orderbookArray[1][1][6]
+    askSize7 = orderbookArray[1][1][7] == null ? 0 : orderbookArray[1][1][7]
+    askSize8 = orderbookArray[1][1][8] == null ? 0 : orderbookArray[1][1][8]
+    askSize9 = orderbookArray[1][1][9] == null ? 0 : orderbookArray[1][1][9]
   }
 
+
+  /**
+   * Allows access to the orderbook's {@code bid}, {@code bidSize}, {@code ask}, {@code
+   * askSize} fields using a three dimensional array. Internally this array is generated
+   * from the bid0 .. bid9, bidSize0 .. bidSize9, ask0 .. ask9, askSize0 .. askSize9
+   * fields i.e. the array itself is dynamically generated.
+   *
+   * Array convention:
+   *
+   * BigDecimal[0][][] = bid
+   * BigDecimal[1][][] = ask
+   *
+   * BigDecimal[][0][] = price
+   * BigDecimal[][1][] = size
+   *
+   * BigDecimal[][][0] = Level 1
+   * BigDecimal[][][1] = Level 2
+   * BigDecimal[][][...] = Level ...
+   * BigDecimal[][][9] = Level 10
+   *
+   * @return a three dimensional array representing the orderbook as described above
+   */
+
   public BigDecimal[][][] getOrderbookArray() {
-    /*
-   BigDecimal[0][][] = bid
-   BigDecimal[1][][] = ask
-
-   BigDecimal[][0][] = price
-   BigDecimal[][1][] = size
-
-   BigDecimal[][][0] = Level 1
-   BigDecimal[][][1] = Level 2
-   BigDecimal[][][...] = Level ...
-   BigDecimal[][][9] = Level 10
-    */
 
     BigDecimal[][][] orderbookArray = new BigDecimal[2][2][10];
 
-    orderbookArray[0][0][0] = bid1
-    orderbookArray[0][0][1] = bid2
-    orderbookArray[0][0][2] = bid3
-    orderbookArray[0][0][3] = bid4
-    orderbookArray[0][0][4] = bid5
-    orderbookArray[0][0][5] = bid6
-    orderbookArray[0][0][6] = bid7
-    orderbookArray[0][0][7] = bid8
-    orderbookArray[0][0][8] = bid9
-    orderbookArray[0][0][9] = bid10
+    orderbookArray[0][0][0] = bid0
+    orderbookArray[0][0][1] = bid1
+    orderbookArray[0][0][2] = bid2
+    orderbookArray[0][0][3] = bid3
+    orderbookArray[0][0][4] = bid4
+    orderbookArray[0][0][5] = bid5
+    orderbookArray[0][0][6] = bid6
+    orderbookArray[0][0][7] = bid7
+    orderbookArray[0][0][8] = bid8
+    orderbookArray[0][0][9] = bid9
 
-    orderbookArray[0][1][0] = bidSize1 == null ? 0 : bidSize1
-    orderbookArray[0][1][1] = bidSize2 == null ? 0 : bidSize2
-    orderbookArray[0][1][2] = bidSize3 == null ? 0 : bidSize3
-    orderbookArray[0][1][3] = bidSize4 == null ? 0 : bidSize4
-    orderbookArray[0][1][4] = bidSize5 == null ? 0 : bidSize5
-    orderbookArray[0][1][5] = bidSize6 == null ? 0 : bidSize6
-    orderbookArray[0][1][6] = bidSize7 == null ? 0 : bidSize7
-    orderbookArray[0][1][7] = bidSize8 == null ? 0 : bidSize8
-    orderbookArray[0][1][8] = bidSize9 == null ? 0 : bidSize9
-    orderbookArray[0][1][9] = bidSize10 == null ? 0 : bidSize10
+    orderbookArray[0][1][0] = bidSize0 == null ? 0 : bidSize0
+    orderbookArray[0][1][1] = bidSize1 == null ? 0 : bidSize1
+    orderbookArray[0][1][2] = bidSize2 == null ? 0 : bidSize2
+    orderbookArray[0][1][3] = bidSize3 == null ? 0 : bidSize3
+    orderbookArray[0][1][4] = bidSize4 == null ? 0 : bidSize4
+    orderbookArray[0][1][5] = bidSize5 == null ? 0 : bidSize5
+    orderbookArray[0][1][6] = bidSize6 == null ? 0 : bidSize6
+    orderbookArray[0][1][7] = bidSize7 == null ? 0 : bidSize7
+    orderbookArray[0][1][8] = bidSize8 == null ? 0 : bidSize8
+    orderbookArray[0][1][9] = bidSize9 == null ? 0 : bidSize9
 
-    orderbookArray[1][0][0] = ask1
-    orderbookArray[1][0][1] = ask2
-    orderbookArray[1][0][2] = ask3
-    orderbookArray[1][0][3] = ask4
-    orderbookArray[1][0][4] = ask5
-    orderbookArray[1][0][5] = ask6
-    orderbookArray[1][0][6] = ask7
-    orderbookArray[1][0][7] = ask8
-    orderbookArray[1][0][8] = ask9
-    orderbookArray[1][0][9] = ask10
+    orderbookArray[1][0][0] = ask0
+    orderbookArray[1][0][1] = ask1
+    orderbookArray[1][0][2] = ask2
+    orderbookArray[1][0][3] = ask3
+    orderbookArray[1][0][4] = ask4
+    orderbookArray[1][0][5] = ask5
+    orderbookArray[1][0][6] = ask6
+    orderbookArray[1][0][7] = ask7
+    orderbookArray[1][0][8] = ask8
+    orderbookArray[1][0][9] = ask9
 
-    orderbookArray[1][1][0] = askSize1 == null ? 0 : askSize1
-    orderbookArray[1][1][1] = askSize2 == null ? 0 : askSize2
-    orderbookArray[1][1][2] = askSize3 == null ? 0 : askSize3
-    orderbookArray[1][1][3] = askSize4 == null ? 0 : askSize4
-    orderbookArray[1][1][4] = askSize5 == null ? 0 : askSize5
-    orderbookArray[1][1][5] = askSize6 == null ? 0 : askSize6
-    orderbookArray[1][1][6] = askSize7 == null ? 0 : askSize7
-    orderbookArray[1][1][7] = askSize8 == null ? 0 : askSize8
-    orderbookArray[1][1][8] = askSize9 == null ? 0 : askSize9
-    orderbookArray[1][1][9] = askSize10 == null ? 0 : askSize10
+    orderbookArray[1][1][0] = askSize0 == null ? 0 : askSize0
+    orderbookArray[1][1][1] = askSize1 == null ? 0 : askSize1
+    orderbookArray[1][1][2] = askSize2 == null ? 0 : askSize2
+    orderbookArray[1][1][3] = askSize3 == null ? 0 : askSize3
+    orderbookArray[1][1][4] = askSize4 == null ? 0 : askSize4
+    orderbookArray[1][1][5] = askSize5 == null ? 0 : askSize5
+    orderbookArray[1][1][6] = askSize6 == null ? 0 : askSize6
+    orderbookArray[1][1][7] = askSize7 == null ? 0 : askSize7
+    orderbookArray[1][1][8] = askSize8 == null ? 0 : askSize8
+    orderbookArray[1][1][9] = askSize9 == null ? 0 : askSize9
 
     return orderbookArray
   }
---
+++ test/unit/org/powertac/common/OrderbookTests.groovy
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2009-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.powertac.common
+
+import grails.test.GrailsUnitTestCase
+
+class OrderbookTests extends GrailsUnitTestCase {
+  protected void setUp() {
+    super.setUp()
+    mockForConstraintsTests(Orderbook)
+  }
+
+  protected void tearDown() {
+    super.tearDown()
+  }
+
+  void testNullableValidationLogic() {
+    Orderbook orderbook = new Orderbook(id: null, bidSize0: null, bidSize1: null, bidSize2: null, bidSize3: null, bidSize4: null, bidSize5: null, bidSize6: null, bidSize7: null, bidSize8: null, bidSize9: null, askSize0: null, askSize1: null, askSize2: null, askSize3: null, askSize4: null, askSize5: null, askSize6: null, askSize7: null, askSize8: null, askSize9: null)
+    assertNull(orderbook.id)
+    assertFalse(orderbook.validate())
+    assertEquals('nullable', orderbook.errors.getFieldError('id').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('competition').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('dateExecuted').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('transactionId').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('product').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('timeslot').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('latest').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('bidSize0').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('bidSize1').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('bidSize2').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('bidSize3').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('bidSize4').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('bidSize5').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('bidSize6').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('bidSize7').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('bidSize8').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('bidSize9').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('askSize0').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('askSize1').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('askSize2').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('askSize3').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('askSize4').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('askSize5').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('askSize6').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('askSize7').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('askSize8').getCode())
+    assertEquals('nullable', orderbook.errors.getFieldError('askSize9').getCode())
+  }
+
+  void testSetOrderbookArray() {
+    Orderbook orderbook = new Orderbook()
+    BigDecimal[][][] orderbookArray = new BigDecimal[2][2][10]
+    orderbookArray[0][0][0] = 1.0
+    orderbookArray[0][0][1] = 2.0
+    orderbookArray[0][0][2] = 3.0
+    orderbookArray[0][0][3] = 4.0
+    orderbookArray[0][0][4] = 5.0
+    orderbookArray[0][0][5] = 6.0
+    orderbookArray[0][0][6] = 7.0
+    orderbookArray[0][0][7] = 8.0
+    orderbookArray[0][0][8] = 9.0
+    orderbookArray[0][0][9] = 10.0
+
+    orderbookArray[1][0][0] = 100.0
+    orderbookArray[1][0][1] = 101.0
+    orderbookArray[1][0][2] = 102.0
+    orderbookArray[1][0][3] = 103.0
+    orderbookArray[1][0][4] = 104.0
+    orderbookArray[1][0][5] = 105.0
+    orderbookArray[1][0][6] = 106.0
+    orderbookArray[1][0][7] = 107.0
+    orderbookArray[1][0][8] = 108.0
+    orderbookArray[1][0][9] = 109.0
+
+    orderbookArray[0][1][0] = 1000.0
+    orderbookArray[0][1][1] = 1001.0
+    orderbookArray[0][1][2] = 1002.0
+    orderbookArray[0][1][3] = 1003.0
+    orderbookArray[0][1][4] = 1004.0
+    orderbookArray[0][1][5] = 1005.0
+    orderbookArray[0][1][6] = 1006.0
+    orderbookArray[0][1][7] = 1007.0
+    orderbookArray[0][1][8] = 1008.0
+    orderbookArray[0][1][9] = 1009.0
+
+    orderbookArray[1][1][0] = 10000.0
+    orderbookArray[1][1][1] = 10001.0
+    orderbookArray[1][1][2] = 10002.0
+    orderbookArray[1][1][3] = 10003.0
+    orderbookArray[1][1][4] = 10004.0
+    orderbookArray[1][1][5] = 10005.0
+    orderbookArray[1][1][6] = 10006.0
+    orderbookArray[1][1][7] = 10007.0
+    orderbookArray[1][1][8] = 10008.0
+    orderbookArray[1][1][9] = 10009.0
+
+    orderbook.setOrderbookArray(orderbookArray)
+
+    assertEquals(1.0, orderbook.bid0)
+    assertEquals(2.0, orderbook.bid1)
+    assertEquals(3.0, orderbook.bid2)
+    assertEquals(4.0, orderbook.bid3)
+    assertEquals(5.0, orderbook.bid4)
+    assertEquals(6.0, orderbook.bid5)
+    assertEquals(7.0, orderbook.bid6)
+    assertEquals(8.0, orderbook.bid7)
+    assertEquals(9.0, orderbook.bid8)
+    assertEquals(10.0, orderbook.bid9)
+
+    assertEquals(100.0, orderbook.ask0)
+    assertEquals(101.0, orderbook.ask1)
+    assertEquals(102.0, orderbook.ask2)
+    assertEquals(103.0, orderbook.ask3)
+    assertEquals(104.0, orderbook.ask4)
+    assertEquals(105.0, orderbook.ask5)
+    assertEquals(106.0, orderbook.ask6)
+    assertEquals(107.0, orderbook.ask7)
+    assertEquals(108.0, orderbook.ask8)
+    assertEquals(109.0, orderbook.ask9)
+
+    assertEquals(1000.0, orderbook.bidSize0)
+    assertEquals(1001.0, orderbook.bidSize1)
+    assertEquals(1002.0, orderbook.bidSize2)
+    assertEquals(1003.0, orderbook.bidSize3)
+    assertEquals(1004.0, orderbook.bidSize4)
+    assertEquals(1005.0, orderbook.bidSize5)
+    assertEquals(1006.0, orderbook.bidSize6)
+    assertEquals(1007.0, orderbook.bidSize7)
+    assertEquals(1008.0, orderbook.bidSize8)
+    assertEquals(1009.0, orderbook.bidSize9)
+
+    assertEquals(10000.0, orderbook.askSize0)
+    assertEquals(10001.0, orderbook.askSize1)
+    assertEquals(10002.0, orderbook.askSize2)
+    assertEquals(10003.0, orderbook.askSize3)
+    assertEquals(10004.0, orderbook.askSize4)
+    assertEquals(10005.0, orderbook.askSize5)
+    assertEquals(10006.0, orderbook.askSize6)
+    assertEquals(10007.0, orderbook.askSize7)
+    assertEquals(10008.0, orderbook.askSize8)
+    assertEquals(10009.0, orderbook.askSize9)
+  }
+
+  void testGetOrderbookArray() {
+    Orderbook orderbook = new Orderbook(bid0: 1.0, bid1: 2.0, bid2: 3.0, bid3: 4.0, bid4: 5.0, bid5: 6.0, bid6: 7.0, bid7: 8.0, bid8: 9.0, bid9: 10.0, ask0: 100.0, ask1: 101.0, ask2: 102.0, ask3: 103.0, ask4: 104.0, ask5: 105.0, ask6: 106.0, ask7: 107.0, ask8: 108.0, ask9: 109.0, bidSize0: 1000.0, bidSize1: 1001.0, bidSize2: 1002.0, bidSize3: 1003.0, bidSize4: 1004.0, bidSize5: 1005.0, bidSize6: 1006.0, bidSize7: 1007.0, bidSize8: 1008.0, bidSize9: 1009.0, askSize0: 10000.0, askSize1: 10001.0, askSize2: 10002.0, askSize3: 10003.0, askSize4: 10004.0, askSize5: 10005.0, askSize6: 10006.0, askSize7: 10007.0, askSize8: 10008.0, askSize9: 10009.0)
+
+    BigDecimal[][][] orderbookArray = orderbook.getOrderbookArray()
+    assertEquals(1.0, orderbookArray[0][0][0])
+    assertEquals(2.0, orderbookArray[0][0][1])
+    assertEquals(3.0, orderbookArray[0][0][2])
+    assertEquals(4.0, orderbookArray[0][0][3])
+    assertEquals(5.0, orderbookArray[0][0][4])
+    assertEquals(6.0, orderbookArray[0][0][5])
+    assertEquals(7.0, orderbookArray[0][0][6])
+    assertEquals(8.0, orderbookArray[0][0][7])
+    assertEquals(9.0, orderbookArray[0][0][8])
+    assertEquals(10.0, orderbookArray[0][0][9])
+
+    assertEquals(100.0, orderbookArray[1][0][0])
+    assertEquals(101.0, orderbookArray[1][0][1])
+    assertEquals(102.0, orderbookArray[1][0][2])
+    assertEquals(103.0, orderbookArray[1][0][3])
+    assertEquals(104.0, orderbookArray[1][0][4])
+    assertEquals(105.0, orderbookArray[1][0][5])
+    assertEquals(106.0, orderbookArray[1][0][6])
+    assertEquals(107.0, orderbookArray[1][0][7])
+    assertEquals(108.0, orderbookArray[1][0][8])
+    assertEquals(109.0, orderbookArray[1][0][9])
+
+    assertEquals(1000.0, orderbookArray[0][1][0])
+    assertEquals(1001.0, orderbookArray[0][1][1])
+    assertEquals(1002.0, orderbookArray[0][1][2])
+    assertEquals(1003.0, orderbookArray[0][1][3])
+    assertEquals(1004.0, orderbookArray[0][1][4])
+    assertEquals(1005.0, orderbookArray[0][1][5])
+    assertEquals(1006.0, orderbookArray[0][1][6])
+    assertEquals(1007.0, orderbookArray[0][1][7])
+    assertEquals(1008.0, orderbookArray[0][1][8])
+    assertEquals(1009.0, orderbookArray[0][1][9])
+
+    assertEquals(10000.0, orderbookArray[1][1][0])
+    assertEquals(10001.0, orderbookArray[1][1][1])
+    assertEquals(10002.0, orderbookArray[1][1][2])
+    assertEquals(10003.0, orderbookArray[1][1][3])
+    assertEquals(10004.0, orderbookArray[1][1][4])
+    assertEquals(10005.0, orderbookArray[1][1][5])
+    assertEquals(10006.0, orderbookArray[1][1][6])
+    assertEquals(10007.0, orderbookArray[1][1][7])
+    assertEquals(10008.0, orderbookArray[1][1][8])
+    assertEquals(10009.0, orderbookArray[1][1][9])
+  }
+}
