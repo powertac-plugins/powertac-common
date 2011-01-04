e435dad8c332761b2903bb4bcbbb7aa8a0f18466
Carsten Block <mail@carstenblock.org>
2011-01-04 15:42:51 +0100

    Tariff constraints refactored and corresponding tests added

--- grails-app/domain/org/powertac/common/Tariff.groovy
+++ grails-app/domain/org/powertac/common/Tariff.groovy
@@ -30,7 +30,7 @@ class Tariff {
   TariffState tariffState
   Boolean isDynamic
   Boolean isNegotiable
-  LocalDateTime dateCreated
+  LocalDateTime dateCreated = new LocalDateTime()
   Boolean latest
 
   BigDecimal signupFee //one-time fee (>0) / reward (<0) charged / paid for contract signup
@@ -107,17 +107,176 @@ class Tariff {
   BigDecimal powerProductionPrice22
   BigDecimal powerProductionPrice23
 
-
   static belongsTo = [competition: Competition, broker: Broker, customer: Customer]
 
   static constraints = {
+    id(nullable: false, blank: false, unique: true)
+    transactionId(nullable: false, blank: false)
+    competition(nullable: false)
+    broker(nullable: false)
+    customer(nullable: true)
+    tariffId(nullable: false)
+    tariffState(nullable: false)
+    isDynamic(nullable: false)
+    isNegotiable(nullablee: false)
+    dateCreated(nullable: false)
+    latest(nullable: false)
+    signupFee(nullable: true, scale: Constants.DECIMALS)
+    earlyExitFee(nullable: true, scale: Constants.DECIMALS)
+    baseFee(nullable: true, scale: Constants.DECIMALS)
+    changeLeadTime(nullable: true)
+    contractStartDate(nullable: true)
+    contractEndDate(nullable: true)
+    minimumContractRuntime(nullable: true)
+    maximumContractRuntime(nullable: true)
+    powerConsumptionThreshold(nullable: true, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionSurcharge(nullable: true, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionThreshold(nullable: true, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionSurcharge(nullable: true, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice0(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice1(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice2(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice3(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice4(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice5(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice6(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice7(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice8(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice9(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice10(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice11(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice12(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice13(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice14(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice15(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice16(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice17(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice18(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice19(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice20(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice21(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice22(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerConsumptionPrice23(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+
+    powerProductionPrice0(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice1(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice2(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice3(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice4(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice5(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice6(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice7(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice8(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice9(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice10(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice11(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice12(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice13(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice14(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice15(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice16(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice17(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice18(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice19(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice20(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice21(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice22(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+    powerProductionPrice23(nullable: false, scale: Constants.DECIMALS, min: 0.0)
+
   }
 
   static mapping = {
     id(generator: 'assigned')
   }
 
+  static transients = ['powerConsumptionPrices', 'powerProductionPrices', 'powerProductionPriceForRange', 'powerConsumptionPriceForRange', 'powerProductionPriceFor', 'powerConsumptionPriceFor', 'flatPowerConsumptionPrice', 'flatPowerProductionPrice']
+
+
   public String toString() {
     return tariffId
+  }
+
+  public BigDecimal[] getPowerConsumptionPrices() {
+    BigDecimal[] array = new BigDecimal[24]
+    for (i in 0..23) {
+      array[i] = this."powerConsumptionPrice$i"
+    }
+    return array
+  }
+
+  public BigDecimal[] getPowerProductionPrices() {
+    BigDecimal[] array = new BigDecimal[24]
+    for (i in 0..23) {
+      array[i] = this."powerProductionPrice$i"
+    }
+    return array
+  }
+
+  public void setPowerConsumptionPrices(BigDecimal[] powerConsumptionPrices) {
+    if (!powerConsumptionPrices) return
+    int length = Math.min(powerConsumptionPrices.length,24)
+    for (i in 0..(length - 1)) {
+      this."powerConsumptionPrice$i" = powerConsumptionPrices[i]
+    }
+  }
+
+  public void setPowerProductionPrices(BigDecimal[] powerProductionPrices) {
+    if (!powerProductionPrices) return
+    int length = Math.min(powerProductionPrices.length, 24)
+    for (i in 0..(length - 1)) {
+      this."powerProductionPrice$i" = powerProductionPrices[i]
+    }
+  }
+
+  public void setFlatPowerProductionPrice(BigDecimal price) {
+    for (i in 0..23) {
+      this."powerProductionPrice$i" = price
+    }
+  }
+
+  public void setFlatPowerConsumptionPrice(BigDecimal price) {
+    for (i in 0..23) {
+      this."powerConsumptionPrice$i" = price
+    }
+  }
+
+  public void setPowerProductionPriceForRange(int start, int end, BigDecimal price) {
+    int i = start < 0 ? 0 : start
+    if (i > 23) i = 23
+    int j = end < i ? i : end
+    if (j > 23) j = 23
+    for (k in i..j) {
+      this."powerProductionPrice$k" = price
+    }
+  }
+
+  public void setPowerConsumptionPriceForRange(int start, int end, BigDecimal price) {
+    int i = start < 0 ? 0 : start
+    if (i > 23) i = 23
+    int j = end < i ? i : end
+    if (j > 23) j = 23
+    for (k in i..j) {
+      this."powerConsumptionPrice$k" = price
+    }
+  }
+
+  public void setPowerProductionPriceFor(int slot, BigDecimal price) {
+    if (slot < 0 || slot > 23) return
+    this."powerProductionPrice$slot" = price
+  }
+
+  public BigDecimal getPowerProductionPriceFor(int slot) {
+    if (slot < 0 || slot > 23) return null
+    return this."powerProductionPrice$slot"
+  }
+
+  public void setPowerConsumptionPriceFor(int slot, BigDecimal price) {
+    if (slot < 0 || slot > 23) return
+    this."powerConsumptionPrice$slot" = price
+  }
+
+  public BigDecimal getPowerConsumptionPriceFor(int slot) {
+    if (slot < 0 || slot > 23) return null
+    return this."powerConsumptionPrice$slot"
   }
 }
---
+++ test/unit/org/powertac/common/TariffTests.groovy
@@ -0,0 +1,317 @@
+/*
+ * Copyright 2009-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.powertac.common
+
+import grails.test.GrailsUnitTestCase
+
+class TariffTests extends GrailsUnitTestCase {
+  protected void setUp() {
+    super.setUp()
+    mockForConstraintsTests(Tariff)
+  }
+
+  protected void tearDown() {
+    super.tearDown()
+  }
+
+  void testNullableValidationLogic() {
+    Tariff tariff = new Tariff(id: null, dateCreated: null)
+    assertFalse(tariff.validate())
+    assertEquals('nullable', tariff.errors.getFieldError('id').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('transactionId').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('competition').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('broker').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('tariffId').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('tariffState').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('isDynamic').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('isNegotiable').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('dateCreated').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('latest').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice0').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice1').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice2').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice3').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice4').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice5').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice6').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice7').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice8').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice9').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice10').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice11').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice12').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice13').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice14').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice15').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice16').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice17').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice18').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice19').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice20').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice21').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice22').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerConsumptionPrice23').getCode())
+
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice0').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice1').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice2').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice3').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice4').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice5').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice6').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice7').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice8').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice9').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice10').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice11').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice12').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice13').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice14').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice15').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice16').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice17').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice18').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice19').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice20').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice21').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice22').getCode())
+    assertEquals('nullable', tariff.errors.getFieldError('powerProductionPrice23').getCode())
+  }
+
+  void testSetAndGetPowerConsumptionPrices() {
+    def price = 15.0
+    Tariff tariff = new Tariff()
+    tariff.setFlatPowerConsumptionPrice(price)
+    BigDecimal[] array = tariff.getPowerConsumptionPrices()
+    assertEquals(24, array.length)
+    for (i in 0..23) {
+      assertEquals(price, tariff."powerConsumptionPrice$i")
+    }
+  }
+
+  void testSetAndGetPowerProductionPrices() {
+    def price = 15.0
+    Tariff tariff = new Tariff()
+    tariff.setFlatPowerProductionPrice(price)
+    BigDecimal[] array = tariff.getPowerProductionPrices()
+    assertEquals(24, array.length)
+    for (i in 0..23) {
+      assertEquals(price, tariff."powerProductionPrice$i")
+    }
+  }
+
+  void testSetPowerConsumptionPricesWithOverlyLongArray() {
+
+    Tariff tariff1 = new Tariff()
+    BigDecimal[] array1 = new BigDecimal[30]
+    array1.eachWithIndex {val, i -> array1[i] = i as BigDecimal}
+    tariff1.setPowerConsumptionPrices(array1)
+
+    BigDecimal[] resultArray = tariff1.getPowerConsumptionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..23) {
+      assertEquals(i as BigDecimal, resultArray[i])
+    }
+  }
+
+  void testSetPowerConsumptionPricesWithShortArray() {
+
+    Tariff tariff1 = new Tariff()
+    BigDecimal[] array1 = new BigDecimal[5]
+    array1.eachWithIndex {val, i -> array1[i] = i as BigDecimal}
+    tariff1.setPowerConsumptionPrices(array1)
+
+    BigDecimal[] resultArray = tariff1.getPowerConsumptionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..4) {
+      assertEquals(i as BigDecimal, resultArray[i])
+    }
+    for (i in 5..23) {
+      assertNull(resultArray[i])
+    }
+  }
+
+
+  void testSetPowerProductionPricesWithOverlyLongArray() {
+
+    Tariff tariff1 = new Tariff()
+    BigDecimal[] array1 = new BigDecimal[30]
+    array1.eachWithIndex {val, i -> array1[i] = i as BigDecimal}
+    tariff1.setPowerProductionPrices(array1)
+
+    BigDecimal[] resultArray = tariff1.getPowerProductionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..23) {
+      assertEquals(i as BigDecimal, resultArray[i])
+    }
+  }
+
+  void testSetPowerProductionPricesWithShortArray() {
+
+    Tariff tariff1 = new Tariff()
+    BigDecimal[] array1 = new BigDecimal[5]
+    array1.eachWithIndex {val, i -> array1[i] = i as BigDecimal}
+    tariff1.setPowerProductionPrices(array1)
+
+    BigDecimal[] resultArray = tariff1.getPowerProductionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..4) {
+      assertEquals(i as BigDecimal, resultArray[i])
+    }
+    for (i in 5..23) {
+      assertNull(resultArray[i])
+    }
+  }
+
+  void testSetPowerConsumptionPriceNullArray() {
+    Tariff tariff1 = new Tariff()
+    tariff1.setPowerConsumptionPrices(null)
+    def resultArray = tariff1.getPowerConsumptionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..23) {
+      assertNull(resultArray[i])
+    }
+  }
+
+  void testSetPowerProductionPriceNullArray() {
+    Tariff tariff1 = new Tariff()
+    tariff1.setPowerProductionPrices(null)
+    def resultArray = tariff1.getPowerProductionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..23) {
+      assertNull(resultArray[i])
+    }
+  }
+
+  void testSetGetPowerConsumptionPriceFor() {
+    Tariff tariff1 = new Tariff()
+    def price = 5.0
+    tariff1.setPowerConsumptionPriceFor(-1, price)
+    tariff1.setPowerConsumptionPriceFor(30, price)
+    def resultArray = tariff1.getPowerConsumptionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..23) {
+      assertNull(resultArray[i])
+    }
+    tariff1.setPowerConsumptionPriceFor(1, price)
+    assertEquals (price, tariff1.powerConsumptionPrice1)
+    assertEquals (price, tariff1.getPowerConsumptionPriceFor(1))
+    assertNull (tariff1.getPowerConsumptionPriceFor(-1))
+    assertNull (tariff1.getPowerConsumptionPriceFor(30))
+  }
+
+  void testSetGetPowerProductionPriceFor() {
+    Tariff tariff1 = new Tariff()
+    def price = 5.0
+    tariff1.setPowerProductionPriceFor(-1, price)
+    tariff1.setPowerProductionPriceFor(30, price)
+    def resultArray = tariff1.getPowerProductionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..23) {
+      assertNull(resultArray[i])
+    }
+    tariff1.setPowerProductionPriceFor(1, price)
+    assertEquals (price, tariff1.powerProductionPrice1)
+    assertEquals (price, tariff1.getPowerProductionPriceFor(1))
+    assertNull (tariff1.getPowerProductionPriceFor(-1))
+    assertNull (tariff1.getPowerProductionPriceFor(30))
+  }
+
+  void testSetGetPowerConsumptionPriceForTooLargeRange() {
+    Tariff tariff1 = new Tariff()
+    def price = 5.0
+    tariff1.setPowerConsumptionPriceForRange(-1, 30, price)
+    def resultArray = tariff1.getPowerConsumptionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..23) {
+      assertEquals(price, resultArray[i])
+    }
+  }
+
+  void testSetGetPowerConsumptionPriceForSubRange() {
+    Tariff tariff1 = new Tariff()
+    def price = 5.0
+    tariff1.setPowerConsumptionPriceForRange(5, 10, price)
+    def resultArray = tariff1.getPowerConsumptionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..4) {
+      assertNull(resultArray[i])
+    }
+    for (i in 5..10) {
+      assertEquals(price, resultArray[i])
+    }
+    for (i in 11..23) {
+      assertNull(resultArray[i])
+    }
+  }
+
+
+  void testSetGetPowerProductionPriceForTooLargeRange() {
+    Tariff tariff1 = new Tariff()
+    def price = 5.0
+    tariff1.setPowerProductionPriceForRange(-1, 30, price)
+    def resultArray = tariff1.getPowerProductionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..23) {
+      assertEquals(price, resultArray[i])
+    }
+  }
+
+   void testSetGetPowerProductionPriceForReverseRange() {
+    Tariff tariff1 = new Tariff()
+    def price = 5.0
+    tariff1.setPowerProductionPriceForRange(35, 10, price)
+     //this should result in first timeslot to be set = 23 and last timeslot to be set = 23
+    def resultArray = tariff1.getPowerProductionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..22) {
+      assertNull(resultArray[i])
+    }
+    assertEquals (price, resultArray[23])
+  }
+
+  void testSetGetPowerConsumptionPriceForReverseRange() {
+    Tariff tariff1 = new Tariff()
+    def price = 5.0
+    tariff1.setPowerConsumptionPriceForRange(35, 10, price)
+     //this should result in first timeslot to be set = 23 and last timeslot to be set = 23
+    def resultArray = tariff1.getPowerConsumptionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..22) {
+      assertNull(resultArray[i])
+    }
+    assertEquals (price, resultArray[23])
+  }
+
+  void testSetGetPowerProductionPriceForSubRange() {
+    Tariff tariff1 = new Tariff()
+    def price = 5.0
+    tariff1.setPowerProductionPriceForRange(5, 10, price)
+    def resultArray = tariff1.getPowerProductionPrices()
+    assertEquals(24, resultArray.length)
+    for (i in 0..4) {
+      assertNull(resultArray[i])
+    }
+    for (i in 5..10) {
+      assertEquals(price, resultArray[i])
+    }
+    for (i in 11..23) {
+      assertNull(resultArray[i])
+    }
+  }
+
+}
