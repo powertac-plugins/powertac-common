d99024db1820f00a29f9c0566101f4ec29caed62
Carsten Block <mail@carstenblock.org>
2011-01-04 16:45:35 +0100

    Version set to 0.2 and joda-time dependency defined to be exactly version 1.1

--- PowertacCommonGrailsPlugin.groovy
+++ PowertacCommonGrailsPlugin.groovy
@@ -1,11 +1,11 @@
 class PowertacCommonGrailsPlugin {
     def groupId = 'org.powertac.plugins'
     // the plugin version
-    def version = "0.1.3"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.6 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ["jodaTime":"1.1 > *"]
+    def dependsOn = ["jodaTime":"1.1"]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
