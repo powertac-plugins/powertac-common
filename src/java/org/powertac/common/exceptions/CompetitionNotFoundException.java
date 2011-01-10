ec8f9b3e9eb16417185fa391f58a67c4bc9a57ec
Carsten Block <mail@carstenblock.org>
2011-01-10 15:22:51 +0100

    ShoutNotFoundException and CompetitionNotFoundException added

---
+++ src/java/org/powertac/common/exceptions/CompetitionNotFoundException.java
@@ -0,0 +1,25 @@
+package org.powertac.common.exceptions;
+
+/**
+ * Thrown if a Competition is Not Found
+ *
+ * @author Carsten Block
+ * @version 1.0, Date: 10.01.11
+ */
+public class CompetitionNotFoundException extends Exception {
+
+  public CompetitionNotFoundException() {
+  }
+
+  public CompetitionNotFoundException(String s) {
+    super(s);
+  }
+
+  public CompetitionNotFoundException(String s, Throwable throwable) {
+    super(s, throwable);
+  }
+
+  public CompetitionNotFoundException(Throwable throwable) {
+    super(throwable);
+  }
+}
---
+++ src/java/org/powertac/common/exceptions/ShoutNotFoundException.java
@@ -0,0 +1,25 @@
+package org.powertac.common.exceptions;
+
+/**
+ * Thrown if a Shout is not found
+ *
+ * @author Carsten Block
+ * @version 1.0, Date: 10.01.11
+ */
+public class ShoutNotFoundException extends Exception {
+
+  public ShoutNotFoundException() {
+  }
+
+  public ShoutNotFoundException(String s) {
+    super(s);
+  }
+
+  public ShoutNotFoundException(String s, Throwable throwable) {
+    super(s, throwable);
+  }
+
+  public ShoutNotFoundException(Throwable throwable) {
+    super(throwable);
+  }
+}
