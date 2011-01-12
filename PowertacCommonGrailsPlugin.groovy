9c586817e7635faf199dda89d1c5dc36089c0a17
Carsten Block <mail@carstenblock.org>
2011-01-12 17:37:25 +0100

    Version set to 0.3.2

--- PowertacCommonGrailsPlugin.groovy
+++ PowertacCommonGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class PowertacCommonGrailsPlugin {
     // the plugin version
-    def version = "0.3.1"
+    def version = "0.3.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.6 > *"
     // the other plugins this plugin depends on
