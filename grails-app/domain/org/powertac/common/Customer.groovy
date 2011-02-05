4102c46be267f70c515e8fd4c81cd5fbb7ba21cf
Carsten Block <mail@carstenblock.org>
2011-02-05 23:12:32 +0100

    Javadoc and automatic competition assignment added to CashUpdate domain class

--- grails-app/domain/org/powertac/common/Customer.groovy
+++ grails-app/domain/org/powertac/common/Customer.groovy
@@ -18,20 +18,51 @@ package org.powertac.common
 
 import org.powertac.common.enumerations.CustomerType
 
+/**
+ * A {@code Customer} instance represents a customer model (i.e. a consumer or a producer)
+ * within a specific competition. The customer data stored is published to all brokers in
+ * the respective competition in order to provide them with an brief overview on what type
+ * of customers participate in the specific competition.
+ *
+ * @author Carsten Block, KIT
+ * @version 1.0 - 04/Feb/2011
+ */
 class Customer implements Serializable {
 
   String id = IdGenerator.createId()
-  Competition competition
+
+  /** Competition this customer is defined for */
+  Competition competition = Competition.currentCompetition()
+
+  /** Name of the customer model */
   String name
-  CustomerType customerType //gives a "rough" classification what type of customer to expect based on an enumeration, i.e. a fixed set of customer types
-  Boolean multiContracting // describes whether or not this customer engages in multiple contracts at the same time
-  Boolean canNegotiate // describes whether or not this customer negotiates over contracts
-  BigDecimal upperPowerCap // >0: max power consumption (think consumer with fuse limit); <0: min power production (think nuclear power plant with min output)
-  BigDecimal lowerPowerCap // >0: min power consumption (think refrigerator); <0: max power production (think power plant with max capacity)
-  BigDecimal carbonEmissionRate // >=0 - gram CO2 per kW/h
-  BigDecimal windToPowerConversion // measures how wind changes translate into load / generation changes of the customer
-  BigDecimal tempToPowerConversion // measures how temperature changes translate into load / generation changes of the customer
-  BigDecimal sunToPowerConversion // measures how sun intensity changes translate into load /generation changes of the customer
+
+  /** gives a "rough" classification what type of customer to expect based on an enumeration, i.e. a fixed set of customer types */
+  CustomerType customerType
+
+  /** describes whether or not this customer engages in multiple contracts at the same time */
+  Boolean multiContracting
+
+  /** describes whether or not this customer negotiates over contracts */
+  Boolean canNegotiate
+
+  /** >0: max power consumption (think consumer with fuse limit); <0: min power production (think nuclear power plant with min output) */
+  BigDecimal upperPowerCap
+
+  /** >0: min power consumption (think refrigerator); <0: max power production (think power plant with max capacity) */
+  BigDecimal lowerPowerCap
+
+  /** >=0 - gram CO2 per kW/h */
+  BigDecimal carbonEmissionRate
+
+  /** measures how wind changes translate into load / generation changes of the customer */
+  BigDecimal windToPowerConversion
+
+  /** measures how temperature changes translate into load / generation changes of the customer */
+  BigDecimal tempToPowerConversion
+
+  /** measures how sun intensity changes translate into load /generation changes of the customer */
+  BigDecimal sunToPowerConversion
 
   //TODO: Possibly add parameters as the ones below that provide descriptive statistical information on historic power consumption / production of the customer
   /*
@@ -43,7 +74,7 @@ class Customer implements Serializable {
 
   static belongsTo = [competition: Competition]
 
-  static hasMany = [meterReadings: MeterReading] //, tariffs: Tariff]
+  static hasMany = [meterReadings: MeterReading]
 
   static constraints = {
     id (nullable: false, blank: false, unique: true)
