438a6c04c5c0bb9059905773481b399065ef560a
Carsten Block <mail@carstenblock.org>
2011-02-06 21:08:08 +0100

    Javadoc updated for shout domain class + automatic competition injection + usage of timeService to determine current "now" time.

--- grails-app/domain/org/powertac/common/Shout.groovy
+++ grails-app/domain/org/powertac/common/Shout.groovy
@@ -16,38 +16,89 @@
 
 package org.powertac.common
 
-import org.joda.time.LocalDateTime
+import org.joda.time.DateTime
 import org.powertac.common.enumerations.BuySellIndicator
 import org.powertac.common.enumerations.ModReasonCode
 import org.powertac.common.enumerations.OrderType
 
-/**
- * A shout domain instance represents an order in the PowerTAC wholesale market.
- * The word "shout" was chosen to avoid db level incompatibilities due to the word
- * "order" being a reserved word in most SQL dialects.^
+ /**
+ * A shout domain instance represents a market or a limit order in the PowerTAC wholesale
+ * market. More precisely it represents a single state of this specific order. Each time a
+ * change occurs, the shout object is cloned, the {@code latest} property is set to false
+ * for the original object (which remains unchanged otherwise) and all necessary changes
+ * are put into the newly cloned shout instance. Like this, each update (e.g. a partial
+ * execution, a deletion by system or by user can be tracked by selecting all
+ * shout instances from the database that carry the same {@code shoutId}, which is the
+ * common identifier for all instances that belong to the same original shout (note that
+ * this is not the {@code id} field, which is different (and unique) for each shout
+ * instance and servers as primary key for the database.
+ *
+ * Note: The word "shout" was chosen to avoid db level incompatibilities due to the word
+ * "order" being a reserved word in most SQL dialects.
+ *
+ * @author Carsten Block
+ * @version 1.0, Feb, 6 2011
  */
 class Shout implements Serializable {
 
+  def timeService
+
   String id = IdGenerator.createId()
-  Competition competition
+
+  /** the competition this shout belongs to */
+  Competition competition = Competition.currentCompetition()
+
+  /** the broker who created this shout */
   Broker broker
+
+  /** the product that should be bought or sold */
   Product product
+
+  /** the timeslot for which the product should be bought or sold */
   Timeslot timeslot
+
+  /** flag that indicates if this shout is a buy or sell order */
   BuySellIndicator buySellIndicator
+
+  /** the product quantity to buy or sell */
   BigDecimal quantity
+
+  /** the limit price, i.e. the max. acceptable buy or min acceptable sell price */
   BigDecimal limitPrice
+
+  /** the last executed quantity (if equal to {@code quantity} the shout is fully executed otherwise it is partially executed */
   BigDecimal executionQuantity
+
+  /** the last execution price */
   BigDecimal executionPrice
+
+  /** either MARKET or LIMIT order */
   OrderType orderType = OrderType.MARKET
-  LocalDateTime dateCreated = new LocalDateTime()
-  LocalDateTime dateMod = new LocalDateTime()
+
+  /** the simulation time when the original shout instance was first created */
+  DateTime dateCreated = timeService.currentTime.toDateTime()
+
+  /** the latest modification time of the shout */
+  DateTime dateMod = timeService.currentTime.toDateTime()
+
+  /** the reason for the latest modifcation to the shout instance */
   ModReasonCode modReasonCode = ModReasonCode.INSERT
+
+  /** A transactionId is generated during the execution of the shout and marks all domain instances in all domain classes that were created or changed during this single transaction (e.g. corresponding transactionLog, CashUpdate, or PositionUpdate instances). Later on this id allows for correlation of the different domain class instances during ex post analysis*/
   String transactionId
+
+  /** marks all shout instances that belong to the same original shout. On every shout change a new clone of the domain instance is generated and shoutId serves as common identifier among all these clones.*/
   String shoutId
+
+  /** optional comment that can be used for example to further describe why a shout was deleted by system (e.g. during deactivaton of a timeslot) */
   String comment
+
+  /** flag that marks the latest shout instance: Purpose: speed up db queries */
   Boolean latest
 
   static belongsTo = [broker: Broker, product: Product, timeslot: Timeslot, competition: Competition]
+
+  static transients = ['timeService']
 
   static constraints = {
     id (nullable: false, blank: false, unique: true)
@@ -88,8 +139,9 @@ class Shout implements Serializable {
    * 1) updates the modReasonCode field in the cloned instance to the value provided as method param
    * 2) sets the 'latest' property in the original instance to false (and persists the change to the db)
    * 3) sets the 'latest' property in the cloned instance to true
-   * 4) sets 'dataMod' property in the cloned instance to *now*
-   * 5) sets 'transactionId' property in the cloned instance to null
+   * 4) keeps the 'dateCreated' property in the cloned instance unchanged
+   * 5) sets 'dateMod' property in the cloned instance to *now* (in simulation time)
+   * 6) sets 'transactionId' property in the cloned instance to null
    *
    * Note: the original shout instance is saved (in order to persist the
    * updated 'latest' field property while the cloned shout instance *is not saved*!!
@@ -110,7 +162,7 @@ class Shout implements Serializable {
     newShout.executionPrice = this.executionPrice
     newShout.orderType = this.orderType
     newShout.dateCreated = this.dateCreated
-    newShout.dateMod = new LocalDateTime()
+    newShout.dateMod = timeService.currentTime.toDateTime()
     newShout.modReasonCode = newModReasonCode
     newShout.transactionId = null
     newShout.shoutId = this.shoutId
