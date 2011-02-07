b2f9b21fc424c8df2bce8a2d50a49f0bcaa29272
Carsten Block <mail@carstenblock.org>
2011-02-07 17:03:13 +0100

    Fix for failing ShoutDoUpdateCmd tests.
    Signed-off-by: Carsten Block <mail@carstenblock.org>

--- test/integration/org/powertac/common/command/ShoutDoUpdateCmdTests.groovy
+++ test/integration/org/powertac/common/command/ShoutDoUpdateCmdTests.groovy
@@ -16,11 +16,12 @@
 
 package org.powertac.common.command
 
+import org.joda.time.DateTime
 import org.powertac.common.enumerations.BuySellIndicator
+import org.powertac.common.enumerations.ModReasonCode
 import org.powertac.common.enumerations.OrderType
 import org.powertac.common.enumerations.ProductType
 import org.powertac.common.*
-import org.powertac.common.enumerations.ModReasonCode
 
 class ShoutDoUpdateCmdTests extends GroovyTestCase {
 
@@ -45,7 +46,7 @@ class ShoutDoUpdateCmdTests extends GroovyTestCase {
     assert (broker2.validate() && broker2.save())
     product = new Product(competition: competition, productType: ProductType.Future)
     assert (product.validate() && product.save())
-    timeslot = new Timeslot(competition: competition, serialNumber: 0)
+    timeslot = new Timeslot(competition: competition, serialNumber: 0, startDateTime: new DateTime(), endDateTime: new DateTime())
     assert (timeslot.validate() && timeslot.save())
     shout = new Shout(competition: competition, product: product, timeslot: timeslot, broker: broker, quantity: 1.0, limitPrice: 10.0, buySellIndicator: BuySellIndicator.BUY, orderType: OrderType.LIMIT, transactionId: 'testTransaction', latest: true, shoutId: 'testShoutId')
     assert (shout.validate() && shout.save())
