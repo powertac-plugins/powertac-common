71f3a269d1d5f7266806b86d0f9bcb2f979c93b7
John Collins <jcollins@cs.umn.edu>
2011-03-19 14:40:44 -0500

    remove auto-scheduling of clock-drive job

--- grails-app/jobs/org/powertac/common/ClockDriveJob.groovy
+++ grails-app/jobs/org/powertac/common/ClockDriveJob.groovy
@@ -1,9 +1,27 @@
+/*
+ * Copyright (c) 2011 by the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.powertac.common
 
-
+/**
+ * Runs the clock. This needs to be scheduled externally to avoid breaking
+ * integration tests.
+ * @author John Collins
+ */
 class ClockDriveJob
 {
-  def timeout = 5000l // execute job once in 5 seconds
   def timeService
 
   // scheduler trigger support
@@ -14,7 +32,7 @@ class ClockDriveJob
    */
   def execute ()
   {
-    println("Clock Updated " + timeService.getCurrentTime())
+    log.info("Clock Updated " + timeService.getCurrentTime())
     timeService.updateTime()
   }
 }
