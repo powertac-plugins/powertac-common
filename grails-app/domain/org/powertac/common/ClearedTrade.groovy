316563392182ebfa304d1634a0c239bd61ebc647
dschnurr <schnurr@iism.uni-karlsruhe.de>
2011-04-04 20:16:35 +0200

    Added ClearedTrade domain class

---
+++ grails-app/domain/org/powertac/common/ClearedTrade.groovy
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2009-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.powertac.common
+
+/**
+ * A clearedTrade instance reports public trade information, i.e. the execution price and quantity.
+ *
+ * It relates to a single transaction, specifying the timeslot and product. The single clearedTrade
+ * instances are aggregated in a collection by the auctioneer before they are sent to other entities/brokers.
+ * In the periodic clearing this collection is collection is built after each clearing and includes
+ * a clearedTrade instance per tradeable timslot and product.
+ *
+ * @author Daniel Schnurr
+ */
+class ClearedTrade implements Serializable {
+
+  String id = IdGenerator.createId()
+
+  /** underlying timeslot for the trade (e.g. for a future the timeslot when real-world exchanges happen)*/
+  Timeslot timeslot
+
+  /** related product that was traded*/
+  Product product
+
+  /** clearing price of the trade */
+  BigDecimal executionPrice
+
+  /** traded quantity of the specified product */
+  BigDecimal executionQuantity
+
+  static constraints = {
+    timeslot(nullable: false, blank: false)
+    product(nullable: false)
+
+    /** price and quantity have to be both not null (regular case) or both null (no execution) */
+    executionPrice(nullable: true, validator: { price, ct ->
+      (ct.executionQuantity && price) || (!ct.executionQuantity && !price)
+    })
+    executionQuantity(nullable: true, validator: { quantity, ct ->
+      (ct.executionPrice && quantity) || (!ct.executionPrice && !quantity)
+    })
+  }
+}
