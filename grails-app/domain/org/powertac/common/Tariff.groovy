5ad925116f92c38ce459a6dd9cc09ef0ff7a45bb
John Collins <jcollins@cs.umn.edu>
2011-05-06 22:56:53 -0500

    handle non-expiring tariff correctly

--- grails-app/domain/org/powertac/common/Tariff.groovy
+++ grails-app/domain/org/powertac/common/Tariff.groovy
@@ -268,7 +268,12 @@ class Tariff
    */
   boolean isExpired ()
   {
-    return timeService.getCurrentTime().millis >= expiration.millis
+    if (expiration == null) {
+      return false
+    }
+    else {
+      return timeService.getCurrentTime().millis >= expiration.millis
+    }
   }
   
   /**
